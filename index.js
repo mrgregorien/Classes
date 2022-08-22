407f4c71e282146b19e60f5699153ea89278f02e
mrgregorien <mrwilliamgregorio@gmail.com>
2022-08-22 01:45:42 -0400

    ...

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 class Rectangle {
+	constructor(height, width){
+		this.height = height;
+		this.width  = width;
+	}
+}
 
+//vs
 
-
+function Rectangle(options) {
+	this.height = options.height;
+	this.width - options.width;
 }
 
